5e98d83074c95e5217a9d5c273e023ab21ed3fcf
Deep Jyoti Chatterjee <deepchatterjee@Deeps-MacBook-Air.local>
2024-05-11 14:08:06 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from './components/Navigation/index';
+import Hero from './components/Hero/index';
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const Navigation = () => {
+    return (
+        <nav className="container">
+        <div className="logo">
+          <img src="./public/images/brand_logo.png" alt="logo" />
+        </div>
+
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    );
+}
+
+export default Navigation;
